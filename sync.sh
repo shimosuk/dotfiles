113542c748a41c0f9fe54385cf69cd357ae896c7
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-10-02 19:04:09 +0900

    dotfile同期用のスクリプト作成

---
+++ sync.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp $HOME/.gitconfig $HOME/dotfiles/dot.gitconfig
+cp $HOME/.gitignore $HOME/dotfiles/dot.gitignore
+cp $HOME/.gitmessage.txt $HOME/dotfiles/dot.gitmessage.txt
+cp $HOME/.vimrc $HOME/dotfiles/dot.vimrc
+cp $HOME/.zshrc $HOME/dotfiles/dot.zshrc

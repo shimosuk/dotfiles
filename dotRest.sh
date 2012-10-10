418cbf62493de4759c39b5c81828d26281116b44
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2012-10-10 09:57:32 +0900

    dotファイル同期後はすぐにzshrcを更新するようにした

--- dotRest.sh
+++ dotRest.sh
@@ -4,3 +4,4 @@ cp $HOME/dotfiles/dot.gitignore $HOME/.gitignore
 cp $HOME/dotfiles/dot.gitmessage.txt $HOME/.gitmessage.txt
 cp $HOME/dotfiles/dot.vimrc $HOME/.vimrc
 cp $HOME/dotfiles/dot.zshrc $HOME/.zshrc
+source ~/.zshrc

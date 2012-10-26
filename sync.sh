c8ffa761af9dd037607b73bd58fcc274e4fb0582
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2012-10-27 02:55:25 +0900

    ファイルごとに細かな設定ができることを学んだ

--- sync.sh
+++ sync.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir $HOME/oldot
+mkdir $HOME/.vim/ftplugin
 mv $HOME/.gitconfig $HOME/oldot/dot.gitconfig
 mv $HOME/.gitignore $HOME/oldot/dot.gitignore
 mv $HOME/.gitmessage.txt $HOME/oldot/dot.gitmessage.txt
@@ -10,3 +11,5 @@ ln -s $HOME/dotfiles/dot.gitignore $HOME/.gitignore
 ln -s $HOME/dotfiles/dot.gitmessage.txt $HOME/.gitmessage.txt
 ln -s $HOME/dotfiles/dot.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/dot.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/dot.vim/filetype.vim $HOME/.vim/                                                                                                                                    [~]
+ln -s $HOME/dotfiles/dot.vim/ftplugin/* $HOME/.vim/ftplugin/
